ee43c09131047fab341c2649088e92ec6f2c1d9e
Priya <guptapriyab@gmail.com>
2020-02-02 21:00:52 +0100

    Updates

---
+++ precache-manifest.e72c041ead95f53be24cb16f52e36fc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "769c41e542d64e1227e86bb8c80d6fdb",
+    "url": "/labelist/index.html"
+  },
+  {
+    "revision": "1bba00913941688b045a",
+    "url": "/labelist/static/css/main.9d68c8a2.chunk.css"
+  },
+  {
+    "revision": "d613a8138f38eca1460d",
+    "url": "/labelist/static/js/2.715876aa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/labelist/static/js/2.715876aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1bba00913941688b045a",
+    "url": "/labelist/static/js/main.895a15e2.chunk.js"
+  },
+  {
+    "revision": "00184fabc1f77ba717ff",
+    "url": "/labelist/static/js/runtime-main.3e1b1ba6.js"
+  }
+]);
