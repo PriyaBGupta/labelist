73210715b56a458968ef11111ad16bdc7e62f276
Priya <guptapriyab@gmail.com>
2020-02-04 00:18:50 +0100

    Added feedback for validation

--- src/components/AddLabel/AddLabel.js
+++ src/components/AddLabel/AddLabel.js
@@ -5,19 +5,35 @@ class AddLabel extends Component{
     state = {
         displayColorPicker: false,
         labelColor: '',
-        labelName: ''
+        labelName: '',
+        labelColorValid: false,
+        labelNameValid: false,
+        labelColorTouched: false,
+        labelNameTouched: false,
+        formValid: false
     }
+    checkValidity(value){
+        return value.trim() !== '';
+    }
+    validateForm() {
+        this.setState({formValid: this.state.labelNameValid && this.state.labelColorValid});
+      }
     handleColorChange = ({hex}) => {
         this.setState({
             labelColor: hex,
-            //displayColorPicker: false
-        });
+            labelColorValid: this.checkValidity(hex),
+            labelColorTouched: true,
+        },this.validateForm);
     }
     handleColorPicker = () => {
         this.setState({displayColorPicker: !this.state.displayColorPicker})
     }
     handleLabel = (event) => {
-        this.setState({labelName: event.target.value});
+        const updatedLabel = event.target.value;
+        this.setState({labelName: updatedLabel,
+             labelNameValid: this.checkValidity(updatedLabel),
+             labelNameTouched: true
+            },this.validateForm);
     }
     render(){
         const popover = {
@@ -29,22 +45,33 @@ class AddLabel extends Component{
             width: '40px',
             height: '40px'
         }
+        const inputClasses = ['form-control'];
+        if(!this.state.labelNameValid && this.state.labelNameTouched){
+            inputClasses.push('is-invalid');
+         }
+         const buttonClasses = ['btn form-control shadow'];
+         if(!this.state.labelColorValid && this.state.labelColorTouched){
+            buttonClasses.push('is-invalid');
+         }
     return(
         <div className="row flex-column px-3">
             <form autoComplete="off">
                 <div className="form-row">
+                <div className="form-group has-danger">
                     <div className="col">
-                        <input type="text" className="form-control" placeholder="Enter Label" onChange={this.handleLabel}/>
-                    </div>
+                        <input type="text" className={inputClasses.join(' ')} placeholder="Enter Label" onChange={this.handleLabel}/>
+                        <div class="invalid-feedback">Enter valid label</div>
+                    </div></div>
+                    <div className="form-group has-danger">
                     <div className="col-auto">
-                        <button type="button" style={swatchSelected} onClick={this.handleColorPicker} className="btn shadow">{this.state.labelColor ? '':<i className="fa fa-eyedropper"></i>}</button>
+                        <button type="button" style={swatchSelected} onClick={this.handleColorPicker} className={buttonClasses.join(' ')}>{this.state.labelColor ? '':<i className="fa fa-eyedropper"></i>}</button>
                         { this.state.displayColorPicker ? <div style={ popover }>
                             <div onClick={ this.handleClose }/>
                             <TwitterPicker onChangeComplete={this.handleColorChange} color={this.state.labelColor}/>
                         </div> : null }
-                    </div>
+                    </div></div>
                     <div className="col-auto">
-                        <button type="button" className="btn btn-primary" onClick={()=>this.props.add(this.state.labelName,this.state.labelColor)}>Add</button>
+                        <button type="button" className="btn btn-primary" disabled={!this.state.formValid} onClick={()=>this.props.add(this.state.labelName,this.state.labelColor)}>Add</button>
                     </div>
                 </div>
             </form>
