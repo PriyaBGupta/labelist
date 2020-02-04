1c1faf8285dd24de382d62b1f43c98c4ef369152
Priya <guptapriyab@gmail.com>
2020-02-04 22:40:17 +0100

    label formated

--- src/ui/Label/Label.js
+++ src/ui/Label/Label.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const Label = ({label,showEdit}) => {
-    return(
-        <div className="row">
-        <div className="col">
-          <h5><span className="badge badge-pill text-white" style={{backgroundColor: label.labelColor}}>{label.labelName}</span></h5>
-        </div>
-        <div className="col-auto">
-          <button type="button" className="btn shadow-sm" onClick={showEdit}><i className="fa fa-pencil"></i></button>
-        </div>
-      </div>
-    )
+const Label = ({ label, showEdit }) => {
+	return (
+		<div className="row">
+			<div className="col">
+				<h5><span className="badge badge-pill text-white" style={{ backgroundColor: label.labelColor }}>{label.labelName}</span></h5>
+			</div>
+			<div className="col-auto">
+				<button type="button" className="btn shadow-sm" onClick={showEdit}><i className="fa fa-pencil"></i></button>
+			</div>
+		</div>
+	)
 }
 export default Label;
